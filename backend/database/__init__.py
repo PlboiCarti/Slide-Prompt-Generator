07b10246de95dc8aa6d23b44da68dbd81e645b6d
PlboiCarti <baodinhsy06@gmail.com>
2026-05-26 09:38:58 +0700

    chore: add root gitignore to block venv

--- backend/database/__init__.py
+++
@@ -1,6 +0,0 @@
-from .connection import get_db, create_tables, Base, database_url, engine, SessionLocal
-
-__all__ = [
-    "get_db", "create_tables", "Base",
-    "database_url", "engine", "SessionLocal",
-]
